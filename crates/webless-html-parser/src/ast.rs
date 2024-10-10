213ad3df3f18639b6e27e81d754b110630ec801c
Alfio <tomarchioalfio5@gmail.com>
2024-10-10 19:30:42 +0100

    Add more HTMLNode types

--- crates/webless-html-parser/src/ast.rs
+++ crates/webless-html-parser/src/ast.rs
@@ -1,10 +1,22 @@
 pub struct HTMLDocument<'a> {
-    html: HTMLNode<'a>,
+    pub html: Box<[HTMLNode<'a>]>,
+}
+
+pub enum DocumentMode {
+    Quirks,
+    Standards,
 }
 
 pub enum HTMLNode<'a> {
+    ///Foreign text, ie. stuff inside XML, JS or CSS nodes, ignored by the parser but different from a regular text node
+    Foreign(&'a str),
+    ///Doctype declaration: <!DOCTYPE html>
+    Doctype(&'a str),
+    ///HTML Comments: <!--This is a comment-->
     Comment(&'a str),
+    ///Regular text
     TextNode(&'a str),
+    ///All other elements
     Element {
         name: &'a str,
         attributes: Box<[HTMLAttribute<'a>]>,
